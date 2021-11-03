02489aca3e476cc2d9fe4c471d04392793b82a25
Tania Tasmin <taniat.kaau@email.com>
2021-11-03 16:42:33 -0400

    Modify the wordsearch function to include vertical searching

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = require('./matrix_transposition');
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  const letters_tran = transpose(letters);
+  const verticalJoin = letters_tran.map(ls => ls.join(''));
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+ 
+  return false;
+
+};
+
+
+
+module.exports = wordSearch;
